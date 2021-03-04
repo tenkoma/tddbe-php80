eebe1a9d68fd7dcb064dca215838c2188e94dd7c
Koji Tanaka <koji.tanaka@binc.jp>
2021-03-04 11:31:15 +0900

    fix: typo

--- money/tests/Unit/MoneyTest.php
+++ money/tests/Unit/MoneyTest.php
@@ -5,13 +5,13 @@ declare(strict_types=1);
 namespace Money\Tests\Unit;
 
 use Money\Tests\TestCase;
-use Money\Doller;
+use Money\Dollar;
 
 class MoneyTest extends TestCase
 {
     public function testMultiplication()
     {
-        $five = new Doller(5);
+        $five = new Dollar(5);
         $five->times(2);
         $this->assertSame(10, $five->amount);
     }
